741c5a0975b22bf00df980d6058a0e552cc6663a
MIKE <eng.miguelaz@gmail.com>
2023-08-14 19:29:29 -0700

    <code>(py):creation of python code variables

---
+++ routine.py
@@ -0,0 +1,23 @@
+''' variables:
+        excercises
+        sets
+        weights
+        reps
+'''
+
+monday_excercise = [ LEGS "4x10"
+        Squats/step front lunges/walking lunges alternate 30lbs
+        Calf Rises 79kg
+    CORE
+        Hanging Leg Rises
+    EXTRA
+        Walk to the river
+        ]
+
+routine = {'monday':monday_excercise}
+
+for day in routine:
+    if input(monday_excercise):
+        print (routine)
+
+
